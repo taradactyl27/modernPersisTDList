e54afcf0b69f4b4de758633fa3359e14374150f5
taradactyl27 <taradactyl27@DESKTOP-DI33A07.localdomain>
2020-03-20 00:19:17 -0400

    added build

---
+++ client/build/precache-manifest.200f4d9c121e890fa67e3e1e9fe2f144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0870f62ebac45357754a58867fe4f91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd10e4e8978a0e25f0e0",
+    "url": "/static/css/main.c426df2b.chunk.css"
+  },
+  {
+    "revision": "dd8d996d6d4b3a9c60e0",
+    "url": "/static/js/2.51c42383.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.51c42383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd10e4e8978a0e25f0e0",
+    "url": "/static/js/main.38790a59.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
